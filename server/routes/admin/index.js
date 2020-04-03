cfc4d0827ef52d16445324a9ff14ac5f7081790d
Bioshaun <ricekent@163.com>
2020-04-03 11:18:07 +0800

    fix upload url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -66,7 +66,7 @@ module.exports = app => {
     upload.single("file"),
     async (req, res) => {
       const file = req.file;
-      file.url = `http://localhost:3000/uploads/${file.filename}`;
+      file.url = `http://test.raymondcarver.xyz/uploads/${file.filename}`;
       res.send(file);
     }
   );
